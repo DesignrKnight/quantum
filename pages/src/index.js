61cd62a226f56adefa54a5f7799761c15b5140d9
DesignrKnight <notify.abelmathew@gmail.com>
2021-10-24 00:58:46 +0530

    Add Auth0

--- pages/src/index.js
+++ pages/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -12,10 +13,16 @@ import './assets/scss/style.scss';
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
-  document.getElementById('root')
+	<Auth0Provider
+		domain="nitrone.eu.auth0.com"
+		clientId="fYN6dlRif5AZ0Yd161L5k6BHVYtagxo9"
+		redirectUri={window.location.origin}
+	>
+		<Router history={history}>
+			<App />
+		</Router>
+	</Auth0Provider>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
